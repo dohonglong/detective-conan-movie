d9eb762c8295cb89b3ace4af6fa05ffb48f442be
Long Do <dohonglong1998@gmail.com>
2024-12-19 18:51:01 +0200

    Set logo on homepage table

--- client/src/components/MovieListPage/MovieListTable.js
+++ client/src/components/MovieListPage/MovieListTable.js
@@ -54,7 +54,7 @@ const MovieListTable = ({ movieList }) => {
                     </Typography>
                   </TableCell>
                   <TableCell align="center" sx={{ padding: "10px" }}>
-                    <img src={movie.logo} alt="logo" width={100} />
+                    <img src={movie.logo} alt="logo" width={100} height={42} />
                   </TableCell>
                   <TableCell align="center">
                     <Typography variant="body1">
